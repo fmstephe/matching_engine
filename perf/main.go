94878300379b58ae0ada6a08488f8f1e975a9eb1
Francis Stephens <francisstephens@gmail.com>
2012-08-19 19:49:56 +0100

    Adding exe for performance profiling

---
+++ perf/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"math/rand"
+	"log"
+	"os"
+	"runtime/pprof"
+	"github.com/fmstephe/matching_engine/matcher"
+)
+
+const (
+	stockId = uint32(1)
+)
+
+var (
+	responseFunc = func(response *matcher.Response) {
+		// Do Nothing
+	}
+)
+
+
+func main() {
+	f, err := os.Create("cpu.prof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	orderNum := 20 * 1000 * 1000
+	sells := mkSells(orderNum, 1000, 1500)
+	buys := mkBuys(orderNum, 1000, 1500)
+	m := matcher.NewMatcher(stockId)
+	pprof.StartCPUProfile(f)
+	for i := 0; i < orderNum; i++ {
+		m.AddBuy(buys[i])
+		m.AddSell(sells[i])
+	}
+	defer pprof.StopCPUProfile()
+}
+
+
+func valRange(n int, low, high int64) []int64 {
+	vals := make([]int64, n)
+	for i := 0; i < n; i++ {
+		vals[i] = rand.Int63n(high-low) + low
+	}
+	return vals
+}
+
+func mkBuys(n int, low, high int64) []*matcher.Order {
+	return mkOrders(n, low, high, matcher.BUY)
+}
+
+func mkSells(n int, low, high int64) []*matcher.Order {
+	return mkOrders(n, low, high, matcher.SELL)
+}
+
+func mkOrders(n int, low, high int64, buySell matcher.TradeType) []*matcher.Order {
+	prices := valRange(n, low, high)
+	orders := make([]*matcher.Order, n)
+	for i, price := range prices {
+		costData := matcher.CostData{Price: price, Amount: 1}
+		tradeData := matcher.TradeData{TraderId: uint32(i), TradeId: uint32(i), StockId: stockId}
+		orders[i] = matcher.NewOrder(costData, tradeData, responseFunc, buySell)
+	}
+	return orders
+}
