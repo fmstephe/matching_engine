d0483c3fd977a456ddfc062bbc52c1f099d9d590
Francis Stephens <francisstephens@gmail.com>
2012-11-12 15:06:26 +0000

    trade/tree.go now fully iterative implementation

--- trade/tree.go
+++ trade/tree.go
@@ -10,22 +10,56 @@ func NewTree() *Tree {
 	return &Tree{}
 }
 
-func (b *Tree) Push(n *Node) {
-	if b.root != nil {
-		b.root.push(n)
-	} else {
-		b.root = n
-		n.pp = &b.root
+func (b *Tree) Push(in *Node) {
+	if b.root == nil {
+		b.root = in
+		in.pp = &b.root
+		return
+	}
+	n := b.root
+	for {
+		switch {
+		case in.val == n.val:
+			n.addLast(in)
+			return
+		case in.val < n.val:
+			if n.left == nil {
+				in.pp = &n.left
+				n.left = in
+				return
+			} else {
+				n = n.left
+			}
+		case in.val > n.val:
+			if n.right == nil {
+				in.pp = &n.right
+				n.right = in
+				return
+			} else {
+				n = n.right
+			}
+		}
 	}
 }
 
 func (b *Tree) PeekMin() *Node {
-	return b.root.peekMin()
+	n := b.root
+	if n == nil {
+		return nil
+	}
+	for {
+		if n.left == nil {
+			return n
+		}
+		n = n.left
+	}
+	panic("Unreachable")
 }
 
 func (b *Tree) PopMin() *Node {
 	if b.root != nil {
-		n := b.root.popMin()
+		n := b.PeekMin()
+		n.pop()
 		n.other.pop() // Clear complementary tree
 		return n
 	}
@@ -33,12 +67,23 @@ func (b *Tree) PopMin() *Node {
 }
 
 func (b *Tree) PeekMax() *Node {
-	return b.root.peekMax()
+	n := b.root
+	if n == nil {
+		return nil
+	}
+	for {
+		if n.right == nil {
+			return n
+		}
+		n = n.right
+	}
+	panic("Unreachable")
 }
 
 func (b *Tree) PopMax() *Node {
 	if b.root != nil {
-		n := b.root.popMax()
+		n := b.PeekMax()
+		n.pop()
 		n.other.pop() // Clear complementary tree
 		return n
 	}
@@ -46,15 +91,35 @@ func (b *Tree) PopMax() *Node {
 }
 
 func (b *Tree) Pop(val int64) *Node {
-	n := b.root.popVal(val)
-	if n != nil {
-		n.other.pop()
+	n := b.get(val)
+	if n == nil {
+		return nil
 	}
+	n.pop()
+	n.other.pop()
 	return n
 }
 
 func (b *Tree) Has(val int64) bool {
-	return b.root.has(val)
+	return b.get(val) != nil
+}
+
+func (b *Tree) get(val int64) *Node {
+	n := b.root
+	for {
+		if n == nil {
+			return nil
+		}
+		if val == n.val {
+			return n
+		}
+		if val < n.val {
+			n = n.left
+		} else {
+			n = n.right
+		}
+	}
+	panic("Unreachable")
 }
 
 type Node struct {
@@ -80,87 +145,6 @@ func initNode(o *Order, val int64, n, other *Node) {
 
 func (n *Node) isHead() bool {
 	return n.pp != nil
-}
-
-func (n *Node) push(in *Node) {
-	switch {
-	case in.val == n.val:
-		n.addLast(in)
-	case in.val < n.val:
-		if n.left == nil {
-			in.pp = &n.left
-			n.left = in
-		} else {
-			n.left.push(in)
-		}
-	case in.val > n.val:
-		if n.right == nil {
-			in.pp = &n.right
-			n.right = in
-		} else {
-			n.right.push(in)
-		}
-	}
-}
-
-func (n *Node) peekMax() *Node {
-	if n == nil {
-		return nil
-	}
-	if n.right == nil {
-		return n
-	}
-	return n.right.peekMax()
-}
-
-func (n *Node) popMax() *Node {
-	m := n.peekMax()
-	m.pop()
-	return m
-}
-
-func (n *Node) peekMin() *Node {
-	if n == nil {
-		return nil
-	}
-	if n.left == nil {
-		return n
-	}
-	return n.left.peekMin()
-}
-
-func (n *Node) popMin() *Node {
-	m := n.peekMin()
-	m.pop()
-	return m
-}
-
-func (n *Node) popVal(val int64) *Node {
-	switch {
-	case n == nil:
-		return nil
-	case val == n.val:
-		n.pop()
-		return n
-	case val < n.val:
-		return n.left.popVal(val)
-	case val > n.val:
-		return n.right.popVal(val)
-	}
-	panic("unreachable")
-}
-
-func (n *Node) has(val int64) bool {
-	if n == nil {
-		return false
-	}
-	if n.val == val {
-		return true
-	}
-	if n.val > val {
-		return n.left.has(val)
-	}
-	return n.right.has(val)
 }
 
 func (n *Node) addLast(in *Node) {
@@ -221,7 +205,13 @@ func (n *Node) detachAll() {
 }
 
 func (n *Node) detachMax() *Node {
-	m := n.peekMax()
+	m := n
+	for {
+		if m.right == nil {
+			break
+		}
+		m = m.right
+	}
 	m.detachAll()
 	return m
 }
