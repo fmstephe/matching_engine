27eda8f316b65f9ede13e545f6f8beba02888ad1
Francis Stephens <francisstephens@gmail.com>
2013-07-15 08:55:24 +0100

    client/ with Client, Trader and TraderMaker types
    The Client is an empty vessel used to hook into the coordinator package
    to provide communication between a large number of traders and a
    matching engine. The Client type is very similar to the
    coordinator/client type used for testing. It contains two channels to
    allow multiple Trader objects to use a single Client for communication.
    The intClient channel is for sending messages to a matching engine and
    the outOfClient channel is for receiving messages back from the matching
    engine. When a Client is created a TraderMaker is also created linked to
    that Client.
    
    The intoClient channel also doubles for registering new traders. We use
    the same channel to prevent race conditions between registration
    messages and messages intended for a matching engine.
    
    The Trader provides a simple interface for submitting trades (buy, sell
    and cancel) which are then forwarded to the connected Client. Traders
    are made by a TraderMaker.
    
    A TraderMaker is created along side a Client and can be used to create
    Traders which are connected to that Client

---
+++ client/client.go
@@ -0,0 +1,102 @@
+package client
+
+import (
+	"fmt"
+	"github.com/fmstephe/matching_engine/msg"
+)
+
+const OUT_BUF_SIZE = 100
+const IN_BUF_SIZE = 100
+
+type registerTrader struct {
+	traderId    uint32
+	outOfClient chan *msg.Message
+}
+
+type Client struct {
+	dispatch   chan *msg.Message
+	appMsgs    chan *msg.Message
+	clientMap  map[uint32]chan *msg.Message
+	intoClient chan interface{}
+}
+
+func NewClient() (*Client, *TraderMaker) {
+	intoClient := make(chan interface{}, IN_BUF_SIZE)
+	return &Client{intoClient: intoClient, clientMap: make(map[uint32]chan *msg.Message)}, &TraderMaker{intoClient: intoClient}
+}
+
+func (c *Client) SetDispatch(dispatch chan *msg.Message) {
+	c.dispatch = dispatch
+}
+
+func (c *Client) SetAppMsgs(appMsgs chan *msg.Message) {
+	c.appMsgs = appMsgs
+}
+
+func (c *Client) Run() {
+	for {
+		select {
+		case m := <-c.appMsgs:
+			clientChan := c.clientMap[m.TraderId]
+			if clientChan == nil {
+				println("Missing traderid", m.TraderId)
+				continue
+			}
+			clientChan <- m
+		case i := <-c.intoClient:
+			switch i := i.(type) {
+			case *registerTrader:
+				if c.clientMap[i.traderId] != nil {
+					panic(fmt.Sprintf("Attempted to register a trader (%i) twice", i.traderId))
+				}
+				c.clientMap[i.traderId] = i.outOfClient
+			case *msg.Message:
+				c.dispatch <- i
+			default:
+				panic(fmt.Sprintf("%T is not a legal type", i))
+			}
+		}
+	}
+}
+
+type TraderMaker struct {
+	intoClient chan interface{}
+}
+
+func (tm *TraderMaker) Make(traderId uint32) *Trader {
+	outOfClient := make(chan *msg.Message, OUT_BUF_SIZE)
+	cr := &registerTrader{traderId: traderId, outOfClient: outOfClient}
+	tm.intoClient <- cr // Performs the registration of this trader
+	return &Trader{traderId: traderId, intoClient: tm.intoClient, outOfClient: outOfClient}
+}
+
+type Trader struct {
+	traderId    uint32
+	intoClient  chan interface{}
+	outOfClient chan *msg.Message
+}
+
+func (t *Trader) Buy(price int64, tradeId, amount, stockId uint32) {
+	t.submit(msg.BUY, price, tradeId, amount, stockId)
+}
+
+func (t *Trader) Sell(price int64, tradeId, amount, stockId uint32) {
+	t.submit(msg.SELL, price, tradeId, amount, stockId)
+}
+
+func (t *Trader) Cancel(price int64, tradeId, amount, stockId uint32) {
+	t.submit(msg.CANCEL, price, tradeId, amount, stockId)
+}
+
+func (t *Trader) submit(kind msg.MsgKind, price int64, tradeId, amount, stockId uint32) {
+	m := &msg.Message{Direction: msg.OUT, Route: msg.APP, Kind: kind, Price: price, Amount: amount, TraderId: t.traderId, TradeId: tradeId, StockId: stockId}
+	if !m.Valid() {
+		panic(fmt.Sprintf("Invalid Message %v", m))
+	}
+	t.intoClient <- m
+}
+
+func (t *Trader) Retrieve() *msg.Message {
+	m := <-t.outOfClient
+	return m
+}
