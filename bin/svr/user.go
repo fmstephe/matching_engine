23de35d705e671c4a523041d0eddd9f6451032c5
Francis Stephens <francisstephens@gmail.com>
2013-11-01 18:15:21 +0000

    stockManager introduced
    We now track the number and kind of stocks that a user owns. The stock
    manager was introduced to track this information and also to prevent a
    user from selling stock that he/she does not own.
    
    The stock manager maintains two distinct sets of information. A set of
    'held' stocks which the user owns. A set of 'to-sell' stocks, which the
    owner owns and are currently part of an un-matched/cancelled sell order.
    
    The process for selling stock:
    
    When a sell order is submitted
    1: Check that the user has the number of stock he/she wishes to sell.
    2: Deduct that number of stocks from the 'held' total and add it to the
       'to-sell' total.
    
    When a sell order is matched (PARTIAL or FULL)
    1: Deduct the number of stocks in the match from the 'to-sell' total.
    
    When a sell order is cancelled
    1: Deduct that number of stocks from the 'to-sell' total and return it
       to the 'held' total.
    
    The process for buying stock:
    
    When a buy order is matched (PARTIAL or FULL)
    1: Add the number of stocks bought to the 'held' total.
    
    Currently a user is given a small number of stocks to begin trading
    with.

--- bin/svr/user.go
+++ bin/svr/user.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/fmstephe/matching_engine/client"
 	"github.com/fmstephe/matching_engine/msg"
+	"strconv"
 )
 
 type webMessage struct {
@@ -16,30 +17,107 @@ type webMessage struct {
 
 type receivedMessage struct {
 	FromClient bool       `json:"fromClient"`
-	Accepted   bool       `json:accepted`
+	Accepted   bool       `json:"accepted"`
 	Message    webMessage `json:"message"`
 }
 
 type response struct {
 	AvailBal    int64           `json:"availBal"`
 	CurBal      int64           `json:"curBal"`
+	Stocks      stockManager    `json:"stocks"`
 	Received    receivedMessage `json:"received"`
 	Outstanding []webMessage    `json:"outstanding"`
+}
+
+// TODO the naming of this hasn't worked out very well
+type stockManager struct {
+	StocksHeld   map[string]uint32 `json:"stocksHeld"`
+	StocksToSell map[string]uint32 `json:"stocksToSell"`
+}
+
+func newStockManager() stockManager {
+	sm := stockManager{}
+	sm.StocksHeld = map[string]uint32{"1": 10, "2": 10, "3": 10}
+	sm.StocksToSell = make(map[string]uint32)
+	return sm
+}
+
+func (sm *stockManager) getKey(stockId uint32) string {
+	return strconv.Itoa(int(stockId))
+}
+
+func (sm *stockManager) cleanup(stockKey string) {
+	if sm.StocksToSell[stockKey] == 0 {
+		delete(sm.StocksToSell, stockKey)
+	}
+	if sm.StocksHeld[stockKey] == 0 {
+		delete(sm.StocksHeld, stockKey)
+	}
+}
+
+func (sm *stockManager) held(stockKey string) uint32 {
+	return sm.StocksHeld[stockKey]
+}
+
+func (sm *stockManager) addHeld(stockKey string, amount uint32) {
+	held := sm.StocksHeld[stockKey]
+	sm.StocksHeld[stockKey] = held + amount
+}
+
+func (sm *stockManager) toSell(stockKey string) uint32 {
+	return sm.StocksToSell[stockKey]
+}
+
+func (sm *stockManager) addToSell(stockKey string, amount uint32) {
+	toSell := sm.StocksToSell[stockKey]
+	sm.StocksToSell[stockKey] = toSell + amount
+}
+
+func (sm *stockManager) canSell(stockId, amount uint32) bool {
+	stockKey := sm.getKey(stockId)
+	return sm.held(stockKey) >= amount
+}
+
+func (sm *stockManager) submitSell(stockId, amount uint32) {
+	stockKey := sm.getKey(stockId)
+	sm.addHeld(stockKey, -amount)
+	sm.addToSell(stockKey, amount)
+	// Don't clean up, we want the zeroed held stocks to remain
+}
+
+func (sm *stockManager) cancelSell(stockId, amount uint32) {
+	stockKey := sm.getKey(stockId)
+	sm.addHeld(stockKey, amount)
+	sm.addToSell(stockKey, -amount)
+	sm.cleanup(stockKey)
+}
+
+func (sm *stockManager) completeSell(stockId, amount uint32) {
+	stockKey := sm.getKey(stockId)
+	sm.addToSell(stockKey, -amount)
+	sm.cleanup(stockKey)
+}
+
+func (sm *stockManager) completeBuy(stockId, amount uint32) {
+	stockKey := sm.getKey(stockId)
+	sm.addHeld(stockKey, amount)
 }
 
 type user struct {
 	curTradeId  uint32
 	availBal    int64
 	curBal      int64
-	clientComm  *client.Comm
+	stocks      stockManager
 	outstanding []webMessage
+	clientComm  *client.Comm
 }
 
 func newUser(clientComm *client.Comm) *user {
 	curTradeId := uint32(1)
 	bal := int64(100)
 	outstanding := make([]webMessage, 0)
-	return &user{curTradeId: curTradeId, availBal: bal, curBal: bal, clientComm: clientComm, outstanding: outstanding}
+	stocks := newStockManager()
+	return &user{curTradeId: curTradeId, availBal: bal, curBal: bal, outstanding: outstanding, stocks: stocks, clientComm: clientComm}
 }
 
 func (u *user) run(msgs chan webMessage, responses chan []byte) {
@@ -49,11 +127,11 @@ func (u *user) run(msgs chan webMessage, responses chan []byte) {
 		var rm receivedMessage
 		select {
 		case wm := <-msgs:
-			rm = u.processOrder(wm)
+			rm = u.processWebMessage(wm)
 		case m := <-outOfClient:
 			rm = u.processMsg(m)
 		}
-		r := &response{Received: rm, AvailBal: u.availBal, CurBal: u.curBal, Outstanding: u.outstanding}
+		r := &response{Received: rm, AvailBal: u.availBal, CurBal: u.curBal, Stocks: u.stocks, Outstanding: u.outstanding}
 		bytes, err := json.Marshal(r)
 		if err != nil {
 			println("Marshalling Error", err.Error())
@@ -63,34 +141,57 @@ func (u *user) run(msgs chan webMessage, responses chan []byte) {
 	}
 }
 
-func (u *user) processOrder(wm webMessage) receivedMessage {
-	if wm.Kind == "CANCEL" {
-		u.clientComm.Cancel(wm.Price, wm.TradeId, wm.Amount, wm.StockId)
-		u.outstanding = append(u.outstanding, wm)
-		return receivedMessage{Message: wm, FromClient: true, Accepted: true}
-	}
-	wm.TradeId = u.curTradeId
-	u.curTradeId++
-	totalCost := wm.Price * int64(wm.Amount)
-	if wm.Kind == "BUY" && totalCost > u.availBal {
+func (u *user) processWebMessage(wm webMessage) receivedMessage {
+	switch wm.Kind {
+	case msg.CANCEL.String():
+		return u.submitCancel(wm)
+	case msg.BUY.String():
+		return u.submitBuy(wm)
+	case msg.SELL.String():
+		return u.submitSell(wm)
+	default:
 		return receivedMessage{Message: wm, FromClient: true, Accepted: false}
 	}
-	if wm.Kind == "BUY" {
-		if err := u.clientComm.Buy(wm.Price, wm.TradeId, wm.Amount, wm.StockId); err != nil {
-			return receivedMessage{Message: wm, FromClient: true, Accepted: false}
-		}
-		u.availBal -= totalCost
-		u.outstanding = append(u.outstanding, wm)
-		return receivedMessage{Message: wm, FromClient: true, Accepted: true}
+}
+
+func (u *user) submitCancel(c webMessage) receivedMessage {
+	if err := u.clientComm.Cancel(c.Price, c.TradeId, c.Amount, c.StockId); err != nil {
+		println("Rejected message: ", err.Error())
+		return receivedMessage{Message: c, FromClient: true, Accepted: false}
 	}
-	if wm.Kind == "SELL" {
-		if err := u.clientComm.Sell(wm.Price, wm.TradeId, wm.Amount, wm.StockId); err != nil {
-			return receivedMessage{Message: wm, FromClient: true, Accepted: false}
-		}
-		u.outstanding = append(u.outstanding, wm)
-		return receivedMessage{Message: wm, FromClient: true, Accepted: true}
+	u.outstanding = append(u.outstanding, c)
+	return receivedMessage{Message: c, FromClient: true, Accepted: true}
+}
+
+func (u *user) submitBuy(b webMessage) receivedMessage {
+	b.TradeId = u.curTradeId
+	u.curTradeId++
+	totalCost := b.Price * int64(b.Amount)
+	if totalCost > u.availBal {
+		return receivedMessage{Message: b, FromClient: true, Accepted: false}
 	}
-	return receivedMessage{Message: wm, FromClient: true, Accepted: false}
+	if err := u.clientComm.Buy(b.Price, b.TradeId, b.Amount, b.StockId); err != nil {
+		println("Rejected message", err.Error())
+		return receivedMessage{Message: b, FromClient: true, Accepted: false}
+	}
+	u.availBal -= totalCost
+	u.outstanding = append(u.outstanding, b)
+	return receivedMessage{Message: b, FromClient: true, Accepted: true}
+}
+
+func (u *user) submitSell(s webMessage) receivedMessage {
+	s.TradeId = u.curTradeId
+	u.curTradeId++
+	if !u.stocks.canSell(s.StockId, s.Amount) {
+		return receivedMessage{Message: s, FromClient: true, Accepted: false}
+	}
+	if err := u.clientComm.Sell(s.Price, s.TradeId, s.Amount, s.StockId); err != nil {
+		println("Rejected message", err.Error())
+		return receivedMessage{Message: s, FromClient: true, Accepted: false}
+	}
+	u.stocks.submitSell(s.StockId, s.Amount)
+	u.outstanding = append(u.outstanding, s)
+	return receivedMessage{Message: s, FromClient: true, Accepted: true}
 }
 
 func (u *user) processMsg(m *msg.Message) receivedMessage {
@@ -110,10 +211,13 @@ func (u *user) cancelOutstanding(c *msg.Message) {
 		if wm.TradeId != c.TradeId {
 			newOutstanding = append(newOutstanding, wm)
 		} else {
-			if wm.Kind == msg.BUY.String() {
+			switch wm.Kind {
+			case msg.BUY.String():
 				// If buy then we need to increase the available balance
 				totalCost := c.Price * int64(c.Amount)
 				u.availBal += totalCost
+			case msg.SELL.String():
+				u.stocks.cancelSell(c.StockId, c.Amount)
 			}
 		}
 	}
@@ -132,9 +236,17 @@ func (u *user) matchOutstanding(m *msg.Message) {
 	for i, wm := range u.outstanding {
 		if wm.TradeId != m.TradeId {
 			newOutstanding = append(newOutstanding, wm)
-		} else if m.Kind == msg.PARTIAL {
-			newOutstanding = append(newOutstanding, wm)
-			newOutstanding[i].Amount -= m.Amount
+		} else {
+			if m.Kind == msg.PARTIAL {
+				newOutstanding = append(newOutstanding, wm)
+				newOutstanding[i].Amount -= m.Amount
+			}
+			if wm.Kind == msg.SELL.String() {
+				u.stocks.completeSell(m.StockId, m.Amount)
+			}
+			if wm.Kind == msg.BUY.String() {
+				u.stocks.completeBuy(m.StockId, m.Amount)
+			}
 		}
 	}
 	u.outstanding = newOutstanding
